8a529f50898a8905563736d0d0bd19380aafcbb2
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2022-09-16 23:03:22 -0500

    fixed routes, changed component to element

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import NasaPhoto from "./components/NasaPhoto";
 import "./App.css";
@@ -8,8 +8,10 @@ function App() {
 	return (
 		<BrowserRouter>
 			<div>
-				<Route component={Home} path="/" exact />
-				<Route component={NasaPhoto} path="/nasaphoto" />
+				<Routes>
+					<Route element={<Home />} path="/" exact />
+					<Route element={<NasaPhoto />} path="/nasaphoto" />
+				</Routes>
 			</div>
 		</BrowserRouter>
 	);
