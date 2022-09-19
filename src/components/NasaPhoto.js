3efc6c25dd4596a2dabc46de059dc1fb2db88a5f
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2022-09-18 23:18:47 -0500

    sent key to .env , added support for video

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 
+const apiKey = process.env.REACT_APP_NASA_KEY;
+
 export default function NasaPhoto() {
 	const [photoData, setPhotoData] = useState(null);
 
@@ -9,12 +11,13 @@ export default function NasaPhoto() {
 
 		async function fetchPhoto() {
 			const res = await fetch(
-				`https://api.nasa.gov/planetary/apod?api_key=caJxz01keFsaffiphz1UHqNMXXQ7rquM2A2xFENI `
+				`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
 			);
 			//going to pull all the data
 			const data = await res.json();
 			//set photo data to whatever we pull out
 			setPhotoData(data);
+			console.log(data);
 		}
 	}, []);
 	//in case photo does not show
@@ -22,7 +25,25 @@ export default function NasaPhoto() {
 
 	return (
 		<div>
-			<img src={photoData.url} alt={photoData.title} />
+			{/* ternary for if either a image or a video  */}
+			{photoData.media_type === "image" ? (
+				<img src={photoData.url} alt={photoData.title} />
+			) : (
+				// iframe imbeds html element inside another html, in this case a video
+				<iframe
+					title="space-video"
+					src={photoData.url}
+					gesture="media"
+					allow="encrypted-media"
+					allowFullScreen
+					className="photo"
+				/>
+			)}
+			<div>
+				<h1>{photoData.title}</h1>
+				<p>{photoData.date}</p>
+				<p>{photoData.explanation}</p>
+			</div>
 		</div>
 	);
 }
